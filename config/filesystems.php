c2ae90617c156eec1984e9d7bf849ccf7a264276
Bolubayo <abayomi.ogunmade@stu.cu.edu.ng>
2025-05-11 11:16:43 +0100

    thirtytwo

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,25 +39,15 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
-        ],
-
-        's3' => [
-            'driver' => 's3',
+            'driver' => 's3', // Using S3 compatible storage
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => env('AWS_DEFAULT_REGION', 'auto'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+            'url' => env('AWS_URL'),
+            'visibility' => 'public',
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
         ],
 
     ],
